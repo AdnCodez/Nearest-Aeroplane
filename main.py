09e8b7f6d37dfc8325f50823c6aeb533b206fd0e
Adnan Habbat <adnan.7.habbat@gmail.com>
2018-08-02 15:43:50 +0100

    Updated Solution

--- main.py
+++ main.py
@@ -1,41 +1,83 @@
-import requests
-import json
-from math import cos, asin, sqrt
+"""
+    File name: test.py
+    Author: AdnHt
+    Date created: 4/20/2018
+    Date last modified: 25/05/2018
+    Python Version: 3.6
+"""
 
+# Challenge link: https://www.reddit.com/r/dailyprogrammer/comments/8i5zc3/20180509_challenge_360_intermediate_find_the/
+# Used links:
+#   https://opensky-network.org/apidoc/rest.html#own-states
+#   https://en.wikipedia.org/wiki/Haversine_formula
+#   https://en.wikipedia.org/wiki/Great-circle_distance
+
+
+import json
+import subprocess
+import sys
+from math import cos, asin, sqrt, pi
+
+# Checking if the requests package exit and install it
+
+package = 'requests'
+
+try:
+    __import__(package)
+except ImportError:
+    try:
+        subprocess.call([sys.executable, '-m', 'pip', 'install', '{}'.format(package)])
+    except SystemExit as e:
+        pass
+
+import requests
+
+# Global vars
+API_LINK = 'https://opensky-network.org/api/states/all'
 lat = float(input('Enter here lat: '))
 lon = float(input('Enter here lon: '))
+# Radius of earth is R = 6371 km R*2 = 12742
+RADIUS = 12742
 
 
-def main(lat, lon):
-    tempDataList = []
+def distance(p, v):
+    # The Haversine formula
+    a = (1 - cos((p['lat'] - v['lat']) * pi)) / 2 + cos(v['lat'] * pi) * cos(p['lat'] * pi) * (
+                1 - cos((p['lon'] - v['lon']) * pi)) / 2
+    return RADIUS * asin(sqrt(a))
+
+
+def nearest_aeroplane(result, lat, lon):
     v = {'lat': lat, 'lon': lon}
+    return min(result, key=lambda p: distance(p, v))
 
-    data = requests.get("https://opensky-network.org/api/states/all")
+
+def load_data():
+    global data
+    tempDataList = {}
+    try:
+        data = requests.get(API_LINK)
+    except requests.exceptions.HTTPError as e:
+        print(' HTTP error: {}'.format(e))
+
     # checking the status of the link above
     # print(data.status_code)
+    if data is not None:
+        tempDataList['content'] = json.loads(data.content)
+        result = []
+        for i in tempDataList['content']['states']:
+            if type(i[5]) is float and type(i[6]) is float:
+                dc = {'lat': i[6], 'lon': i[5]}
+                result.append(dc)
+        tempDataList['result'] = result
+        return tempDataList
 
-    data = json.loads(data.content)
-    # print(data)
-    states = data['states']
 
-    for i in states:
-        if type(i[5]) is float and type(i[6]) is float:
-            dc = {'lat': i[6], 'lon': i[5]}
-            tempDataList.append(dc)
-
-    def distance(lat1, lon1, lat2, lon2):
-        p = 0.017453292519943295  # pi / 180
-        # The Haversine formula
-        a = (1 - cos((lat2 - lat1) * p)) / 2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
-        return 12742 * asin(sqrt(a))  # Radius of earth is R = 6371 km R*2 = 12742
-
-    def closest(data, v):
-        return min(data, key=lambda p: distance(v['lat'], v['lon'], p['lat'], p['lon']))
-
-    var = closest(tempDataList, v)
-    # print(var)
+def result(lat, lon):
+    data = load_data()
+    var = nearest_aeroplane(data['result'], lat, lon)
     res = ''
-    for i in states:
+    for i in data['content']['states']:
         if var['lat'] == i[6] and var['lon'] == i[5]:
             res += 'Callsign: {}\n'.format(i[1])
             res += 'Lattitude: {}\n'.format(i[6])
@@ -47,12 +89,9 @@ def main(lat, lon):
     return res
 
 
-print(main(lat, lon))
-
-# Challenge link: https://www.reddit.com/r/dailyprogrammer/comments/8i5zc3/20180509_challenge_360_intermediate_find_the/
-# Used links:
-#   https://opensky-network.org/apidoc/rest.html#own-states
-#   https://en.wikipedia.org/wiki/Haversine_formula
-#   https://en.wikipedia.org/wiki/Great-circle_distance
+def main():
+    print(result(lat, lon))
 
 
+if __name__ == "__main__":
+    main()
